774c1d143609c155916877209b6efe0131595fa1
HamidRezaRezaeiGitHub <hamidreza74hrr@yahoo.com>
2023-03-15 11:28:36 -0400

    challenges/java-datatypes added

---
+++ src/main/java/com/hrr/hackerRank/javaChallenges/basic/easy/introduction/DataTypes.java
@@ -0,0 +1,31 @@
+package com.hrr.hackerRank.javaChallenges.basic.easy.introduction;
+
+import java.io.InputStream;
+import java.util.Scanner;
+
+public class DataTypes {
+    //    https://www.hackerrank.com/challenges/java-datatypes/problem?isFullScreen=true
+    public static void main(String[] argh) {
+        InputStream is = DataTypes.class.getClassLoader().getResourceAsStream("DataTypes.txt");
+        Scanner sc = new Scanner(is);
+//        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+
+        for (int i = 0; i < t; i++) {
+
+            try {
+                long x = sc.nextLong();
+                System.out.println(x + " can be fitted in:");
+                if (x >= -128 && x <= 127) System.out.println("* byte");
+                //Complete the code
+                if (x >= -32768 && x <= 32767) System.out.println("* short");
+                if (x >= Math.pow(2, 31) * (-1) && x <= Math.pow(2, 31) - 1) System.out.println("* int");
+                if (x >= Math.pow(2, 63) * (-1) && x <= Math.pow(2, 63) - 1) System.out.println("* long");
+            } catch (Exception e) {
+                System.out.println(sc.next() + " can't be fitted anywhere.");
+            }
+
+        }
+    }
+
+}
