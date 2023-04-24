af4074019da1990ba4b4338fe947631e43a11c2a
HamidRezaRezaeiGitHub <hamidreza74hrr@yahoo.com>
2023-04-24 16:13:22 -0400

    challenges/java-generics added

---
+++ src/main/java/com/hrr/hackerRank/javaChallenges/basic/easy/dataStructure/Generic1.java
@@ -0,0 +1,32 @@
+package com.hrr.hackerRank.javaChallenges.basic.easy.dataStructure;
+
+import java.lang.reflect.Method;
+
+public class Generic1 {
+//    https://www.hackerrank.com/challenges/java-generics/problem?isFullScreen=true
+
+    public static void main(String args[]) {
+        Printer myPrinter = new Printer();
+        Integer[] intArray = {1, 2, 3};
+        String[] stringArray = {"Hello", "World"};
+        myPrinter.printArray(intArray);
+        myPrinter.printArray(stringArray);
+        int count = 0;
+
+        for (Method method : Printer.class.getDeclaredMethods()) {
+            String name = method.getName();
+            if (name.equals("printArray"))
+                count++;
+        }
+        if (count > 1) System.out.println("Method overloading is not allowed!");
+    }
+}
+
+class Printer {
+    //Write your code here
+    public <T> void printArray(T[] arr) {
+        for (T t : arr) {
+            System.out.println(t);
+        }
+    }
+}
